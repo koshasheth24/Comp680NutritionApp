ed40336a8b53177a6a34810fcda95971228c05c3
koshasheth24 <kosha.sheth.163@my.csun.edu>
2017-03-12 22:28:56 -0700

    Flow

--- app/src/main/java/com/example/kosha/comp680nutritionapp/Login.java
+++ app/src/main/java/com/example/kosha/comp680nutritionapp/Login.java
@@ -38,8 +38,6 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
-        databaseHelper=new DatabaseHelper(activity);
-        databaseHelper.getConnection();
         initViews();
         initListeners();
         initObjects();
